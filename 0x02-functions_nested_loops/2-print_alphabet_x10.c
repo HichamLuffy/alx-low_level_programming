a52ad7ef8a313e6af4a8b1a62bff0709afd7aa29
test2 <test2@gmail.com>
2023-03-21 10:04:59 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_alphabet - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	while ( i < 10 )
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		i++;
+		_putchar('\n');
+	}
+
+}
