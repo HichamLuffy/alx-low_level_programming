d4e504217626c916312b9638ab9c8d37af2f13fb
test2 <test2@gmail.com>
2023-03-21 17:27:40 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,21 +6,38 @@
  */
 void print_to_98(int n)
 {
-	int num, i;
+	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n >= 0 && n <= 98)
 	{
-		num = i;
-		do
+		for (i = n; i <= 98; i++)
 		{
-			_putchar(num % 10 + '0');
-			num /= 10;
-		} while (num != 0);
-		if (i != 98)
-		{
-		_putchar(',');
-		_putchar(' ');
+			printf("%d", i);
+			if (i != 98)
+				printf(',');
 		}
+		printf("%n")
 	}
-	_putchar('\n');
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(",");
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(",");
+		}
+		printf("\n")
+	}
+	else if (n == 98)
+		printf("%d", n);
 }
