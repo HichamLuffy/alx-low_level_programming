128fe10d2b688fc9eeb1ce9b6630b80a8ac4d35f
test2 <test2@gmail.com>
2023-03-21 19:08:06 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,11 +10,11 @@ int main(void)
 
 	num1 = 1;
 	num2 = 2;
-	printf("%d, %d, ", num1, num2);
+	printf("%ld, %ld, ", num1, num2);
 	for (i = 3; i <= 50; i++)
 	{
 		nextnum = num1 + num2;
-		printf(", %d", nextnum);
+		printf(", %ld", nextnum);
 		num1 = num2;
 		num2 = nextnum;
 	}
