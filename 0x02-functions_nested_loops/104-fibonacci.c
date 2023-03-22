34d3e81d92027b488fcff2f871b84b330337a6b0
test2 <test2@gmail.com>
2023-03-21 19:49:57 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,20 +3,16 @@
 int main()
 {
 	int a = 1, b = 2, c, count = 0;
-	printf("%d, %d, ", a, b);
+
+	printf("%d, %d", a, b);
 	count += 2;
 	while (count < 98) {
 		c = a + b;
-		printf("%d", c);
-		if (count < 97) {
-			printf(", ");
-		}
+		printf(", %d", c);
 		a = b;
 		b = c;
 		count++;
 	}
 	printf("\n");
-
-
 	return 0;
 }
