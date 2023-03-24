83ea76af9e56b94bb3df53b5db4e668f47b1037e
test2 <test2@gmail.com>
2023-03-23 19:20:02 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,20 +9,22 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	i = 1;
+	printf("%d", i);
+	for (i = 2; i < 101; i++)
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		} else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		} else if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		} else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 	}
 	return (0);
