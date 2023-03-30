c062f186a46b82011308a3f6bd81150f9beeac8f
test2 <test2@gmail.com>
2023-03-29 18:52:35 -0700

    main.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,30 @@
+fndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char _strcat(chardest, char src);
+
+char_strncat(char dest, charsrc, int n);
+
+char _strncpy(chardest, char src, int n);
+
+int _strcmp(chars1, char s2);
+
+void reverse_array(inta, int n);
+
+char string_toupper(char);
+
+char cap_string(char);
+
+char leet(char);
+
+char rot13(char);
+
+void print_number(int n);
+
+char infinite_add(charn1, char n2, charr, int size_r);
+
+void print_buffer(char b, int size);
+
+#endif / MAIN_H */
