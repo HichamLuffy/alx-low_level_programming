c7272a02642f051f445532590db4102605baa919
test2 <test2@gmail.com>
2023-04-05 17:34:36 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - check the code
+ *
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if(!*s)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
