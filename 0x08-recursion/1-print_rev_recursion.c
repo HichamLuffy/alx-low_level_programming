a495002497950684b1659187793e6c0be0bbe8c6
test2 <test2@gmail.com>
2023-04-05 17:12:23 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * void _print_rev_recursion - check the code
  *@s: parametre
@@ -7,7 +7,7 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 		return;
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
