d29e87130248b021c7720448ce4be73b742bf98b
test2 <test2@gmail.com>
2023-04-08 16:57:17 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *@argc: parametre
+ *@argv: parametre
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc);
+        return (0);
+}
