41ec91b84348ad7f379e86cfda394eefda23df33
test2 <test2@gmail.com>
2023-04-08 16:48:28 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *@argc: parametre
+ *@argv: parametre
+ * Return: Always 0.
+ */
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
