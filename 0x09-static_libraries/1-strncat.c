6d68324f5d53becfb266e860fd6a480ec83bc6c6
test2 <test2@gmail.com>
2023-04-07 17:29:53 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- *_strcat - check the code
+ *_strncat - check the code
  *@dest: parametre
  *@src: parametre
+ *@n: parametre
  *Return: Always 0.
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
         int j, i = 0;
 
         while (dest[i] != '\0')
                 i++;
         j = 0;
-        while (src[j] != '\0')
+        while (j < n && src[j] != '\0')
         {
                 dest[i] = src[j];
                 i++;
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+/**
+ *_abs - check the code
+ *@i: parametre
+ *Return: Always 0.
+ */
+int _abs(int i)
+{
+        if (i >= 0)
+        {
+                return (i);
+        }
+        else
+        {
+                return (i * -1);
+        }
+}
