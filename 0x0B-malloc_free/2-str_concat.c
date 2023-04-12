23a538ad77fb1cf5b6b4d3a0299c21f15e7f172a
test2 <test2@gmail.com>
2023-04-11 18:57:19 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - check the code
+ * @s1: parametre
+ * @s2: parametre
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *som;
+	int i, k;
+
+	if (!s1 && !s2)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	k = 0;
+	while (s2[k] != '\0')
+		k++;
+	som = malloc(sizeof(char) * (i + k + 2));
+	if (som == NULL)
+		return (NULL);
+	i = 0;
+	k = 0;
+	while (s1[i] != '\0')
+	{
+		som[i] = s1[i];
+		i++;
+	}
+	while (s2[k] != '\0')
+	{
+		som[i] = s2[k];
+		i++;
+		k++;
+	}
+	som[i] = '\0';
+		return (som);
+}
