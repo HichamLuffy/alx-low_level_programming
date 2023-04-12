05f2b0680c224adca1f3d94cdb6c6c0ae56e250b
test2 <test2@gmail.com>
2023-04-11 18:05:56 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,9 @@ char *_strdup(char *str)
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-	a = malloc(sizeof(char) * i);
+	a = malloc(sizeof(char) * (i + 1));
+	if (a == NULL)
+		return (NULL);
 	for (j = 0; str[j]; j++)
 		a[j] = str[j];
 	return (a);
