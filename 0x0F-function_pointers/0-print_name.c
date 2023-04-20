7107c92a020340a84b6b913b7ca0634959046379
test2 <test2@gmail.com>
2023-04-19 17:33:11 -0700

    task 2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
+ * print_name - check the code
+ * @name: parametre
+ * @f: parametre
  * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - check the code
+ *@array: parametre
+ *@size: parametre
+ *@cmp: parametre
+ * Return: Always 0.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i <= size; i++)
+	{
+		cmp(array[i]);
+		return (i);
+	}
+}
+
