572f320fb59b2a487a99489440dd59bbd440963b
hicham <hicham@gmail.com>
2023-04-27 17:23:02 -0700

    task5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+void task5(void) __attribute__ ((constructor));
 /**
  * task5 - check the code
  *
@@ -7,5 +8,6 @@
  */
 void task5(void)
 {
-	printf(You're beat! and yet, you must allow,\nI bore my house upon my back!\n);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
