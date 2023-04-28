63c8f65c43343ca609b3aa156a01d060a18c843a
hicham <hicham@gmail.com>
2023-04-27 17:17:32 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * task5 - check the code
+ *
+ * Return - void
+ */
+void task5(void)
+{
+	printf(You're beat! and yet, you must allow,\nI bore my house upon my back!\n);
+}
