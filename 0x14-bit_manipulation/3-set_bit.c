b93698930e9029dac8d95b5bec7655135661f226
hicham <hicham@gmail.com>
2023-05-04 13:40:09 -0700

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * set_bit - check the code
+ * n: parametre
+ * index: parametre
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit;
+
+	if (index > 63)
+		return (-1);
+	bit = 1UL << index;
+	*n |= bit;
+	return (1);
+}
