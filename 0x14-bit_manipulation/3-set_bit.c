8f0f51d30f53982fc518fc261ea5f1fed1e0c1e2
hicham <hicham@gmail.com>
2023-05-04 13:41:57 -0700

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * set_bit - check the code
- * n: parametre
- * index: parametre
+ * @n: parametre
+ * @index: parametre
+ * Return: 1 || 0
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
