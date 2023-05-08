3cb7aab4e88f5c9e17621c926a084ef57043f85e
hicham <hicham@gmail.com>
2023-05-08 13:09:14 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fp = open(filename, O_RDONLY);
 	if (fp == -1)
 		return (0);
-	a = malloc(sizeof(char)*letters);
+	a = malloc(sizeof(char) * letters);
 	b = read(fp, a, letters);
 	c = write(STDOUT_FILENO, a, b);
 
