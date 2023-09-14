a8de3cd548c8d381c9d6aafc26bb61c68abb6012
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-15 00:52:56 +0100

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,7 +19,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(tmp);
 		return (1);
 	}
-	while (index > 1 && tmp)
+	while (index > 0 && tmp)
 	{
 		tmp = tmp->next;
 		index--;
