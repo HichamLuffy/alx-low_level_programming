2c944ea61ae1836a30c46b583caba3b37efa00d8
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-15 00:52:11 +0100

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -4,7 +4,7 @@
  * @head: head of the list
  * @index: index of the node to delete
  * Return: 1 on success, -1 on failure
-*/
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
@@ -26,7 +26,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	if (!tmp)
 		return (-1);
-	tmp->prev->next = tmp->next;
+	if (tmp->prev)
+		tmp->prev->next = tmp->next;
 	if (tmp->next)
 		tmp->next->prev = tmp->prev;
 	free(tmp);
