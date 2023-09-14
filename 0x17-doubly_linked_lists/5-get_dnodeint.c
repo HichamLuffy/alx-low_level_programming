5beca23aed0df24cf9c363dcedc0cbe57ec510df
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-14 23:32:11 +0100

    task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
  * @head: head of the list
  * @index: index of the node
  * Return: nth node
-*/
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *tmp;
@@ -16,7 +16,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (head);
 
 	tmp = head;
-	while (index > 1)
+	while (index > 0)
 	{
 		tmp = tmp->next;
 		index--;
