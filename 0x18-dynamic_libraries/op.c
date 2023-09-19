b518904b22efaf6058330b9ba4f385bd8a4a4c06
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-19 01:23:42 +0100

    task 2

---
+++ 0x18-dynamic_libraries/op.c
@@ -0,0 +1,59 @@
+int add(int i, int j);
+int sub(int i, int j);
+int mul(int i, int j);
+int div(int i, int j);
+int mod(int i, int j);
+
+/**
+ * div - check the code
+ * @i: parametre
+ * @j: parametre
+ */
+int div(int i, int j)
+{
+	return (i / j);
+}
+/**
+ * mod - check the code
+ * @i: parametre
+ * @j: parametre
+ */
+int mod(int i, int j)
+{
+	return 0;
+}
+/**
+ * add - adds two integers
+ * @i: parametre
+ * @j: parametre
+ * Return: the sum
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
+
+/**
+ * sub - checks the code
+ * @i: parametre
+ * @j: parametre
+ * Return: the subtract
+ */
+
+int sub(int i, int j)
+{
+	return (i - j);
+}
+
+/**
+ * mul - multiplies two integers
+ * @i: parametre
+ * @j: parametre
+ * Return: the multiple
+ */
+
+int mul(int i, int j)
+{
+	return (i * j);
+}
