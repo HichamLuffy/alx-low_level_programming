08d225e0b66758e7c8f474128764d2d1f82ccc9b
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-06 03:21:00 +0100

    tests

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,7 +4,7 @@
  * @ht: hash table
  * @key: key
  * Return: value
-*/
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,4 +1,23 @@
 #include "hash_tables.h"
+
+void free_node(hash_node_t *node)
+{
+    free(node->value);
+}
+void free_table(hash_table_t *ht)
+{
+    hash_node_t *tmp;
+    unsigned long int i = 0;
+    for (i = 0; i < ht->size; i++)
+    {
+        tmp = ht->array[i];
+        if (tmp)
+            free_node(tmp);
+    }
+    free(ht->array);
+    free(ht);
+}
+
 /**
  * hash_table_delete - deletes a hash table
  * @ht: hash table
@@ -26,6 +45,7 @@ void hash_table_delete(hash_table_t *ht)
 		i++;
 
 	}
-	free(ht->array);
-	free(ht);
+    free(ht->array);
+    free_table(ht);
+    free(ht);
 }
