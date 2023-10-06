83767303a8e6bfc3f8c13a35ef22eeca1e506fb1
HichamLuffy <imalxvssteve2020@gmail.com>
2023-10-06 02:02:40 +0100

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,1 +1,27 @@
-#
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		if (node)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+
+		}
+
+
+	}
+	printf("}\n");
+}
